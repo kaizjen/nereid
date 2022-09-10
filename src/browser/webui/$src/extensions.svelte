e3aea07bc5f0ec7195bdc0b6e4453af0662b3bf1
wheezard <90904039+wheezard@users.noreply.github.com>
2022-09-10 16:08:55 +0400

    feat?: nereid://extensions unimplemented notice

---
+++ src/browser/webui/$src/extensions.svelte
@@ -0,0 +1,20 @@
+<script>
+  import Header from "./common/Header.svelte";
+  import * as Icons from "./icons.js";
+
+  const { t } = window.nereid.i18n;
+</script>
+
+<svelte:head>
+  <title>{t('common.extensions')}</title>
+</svelte:head>
+
+<header>
+  <Header name="extensions">
+    <Icons.Extensions />
+  </Header>
+</header>
+
+<h3>
+  {t('unimplemented')}
+</h3>
