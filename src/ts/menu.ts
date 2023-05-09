22d336fa8a0b4bb6d3e67800b81e4465b35a3b84
wheezard <90904039+wheezard@users.noreply.github.com>
2023-05-09 22:07:10 +0400

    fix?: made the new system work for `Ctrl`+`P` (private tabs)

--- src/ts/menu.ts
+++ src/ts/menu.ts
@@ -189,10 +189,26 @@ const tabs_windows: Electron.MenuItemConstructorOptions[] = [
     click(_m, win) {
       if (!isTabWindow(win)) return;
 
-      createTab(win, {
-        url: 'nereid://private',
-        private: true
-      })
+      if (config.get().behaviour.keyboardOpensTabNearby) {
+        createTab(win, {
+          url: $.newTabUrl,
+          position: win.tabs.indexOf(win.currentTab) + 1,
+          private: true
+        })
+
+      } else {
+        const tabGroup = getTabGroupByTab(win.currentTab);
+        if (tabGroup && config.get().ui.onlyShowCurrentTabGroup) {
+          // In this case, open the tab at the end of the tab group
+          createTab(win, {
+            url: $.newTabUrl,
+            position: tabGroup.endIndex,
+            groupID: tabGroup.id,
+            private: true
+          })
+
+        } else createTab(win, { url: $.newTabUrl, private: true });
+      }
       focusChrome(win)
     },
     accelerator: 'CmdOrCtrl+P',
